0e889afd0368c4ef348eda695176c71aeb5dc87a
Joshua Cidoni-Walker <32071430+jcidoniwalker@users.noreply.github.com>
2018-07-01 17:57:23 -0400

    Update rest_json_parser.php

--- rest_json_parser.php
+++ rest_json_parser.php
@@ -1,81 +1,79 @@
 <?php
+    $quoteCurrency = "USD"; // The currency you want the coins to be quoted in (applies to price, marketcap, volume, percent change, etc.)
+    $sleepPeriod = 3; // CoinMarketCap allows 30 requests per minute -- Adjust this to avoid rate-limiting (the more coins, the higher the sleep period should be)
 
-/* 
-Joshua Cidoni
-This is a simple script that will fetch a JSON decoded array from a REST API (source: coinmarketcap.com)
-It will then prepare data for entry (with protection from SQL injections) into a MySQL database (coin name, coin price, current unix timestamp) using OOP-MySQLi constructs.
-09/23/2017
-*/
+    $i = 0; // Used for our while loop
+    do {
+        if(is_int($i / 100)) { // When $i = 100, 200, 300 condition
+            $startParam = $i + 1; // Start all indexing + 1 because CMC starts listing id's from 1, not 0
+            $cmcRequest = GetJSON_Array("https://api.coinmarketcap.com/v2/ticker/?start=" . $startParam . "&convert=" . $quoteCurrency); // Get and decode the JSON array from CMC's API link
+            $numCoins = ceil($cmcRequest['metadata']['num_cryptocurrencies'] / 100) * 100; // Round up to the nearest hundred, eg. (Number of coins on CMC: 1439 - $numCoins after ceil = 1500)
+            
+            foreach($cmcRequest['data'] as $key=>$value) {
+                // Print the name and price of the resultset (for each coin) looped through) - this is for demonstration purposes, please read the next multi-line comment
+                print $value['name'] . " - " . $value['quotes'][$quoteCurrency]['price'] . "\n";
 
-function ConnectTo_MYSQL($host, $username, $password, $db, $port) { // Returns a succesful MySQLi Object, or stops the program
-	
-	$mysqli = new mysqli($host, $username, $password, $db, $port);
-	if ($mysqli->connect_errno) {
-		die ("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") \n" . $mysqli->connect_error);
-	}
-	
-	return $mysqli;
-}
+                /*
+                    This foreach loop is a blank canvas (intentionally). Inside this loop is every coin's dataset from
+                    the API. A copy of the structure is provided below--and, is subject to change.
 
-function GetJSON_Array($url) { // Fetches a JSON decoded array from a given URL, if unsuccessful it will stop the program
-	
-	$ch = curl_init();
-	
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_USERAGENT, "Parser");
-	
-	$ch_headers = ['Accept:application/json'];
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $ch_headers);
-	
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    You can do here whatever it is that you would like to do with the results of ALL coins
+                    provided by CoinMarketCap's API. Have fun!
 
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,  2);
-	
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+                    Example of result set in this loop:
 
-	$response = curl_exec($ch);
-	
-	if(curl_errno($ch)) {
-		print curl_error($ch) . "\n";
-		die();
-	 }
-	 
-	 return json_decode($response,true);
-	
-}
+                        "id": 1, 
+                        "name": "Bitcoin", 
+                        "symbol": "BTC", 
+                        "website_slug": "bitcoin", 
+                        "rank": 1, 
+                        "circulating_supply": 17126087.0, 
+                        "total_supply": 17126087.0, 
+                        "max_supply": 21000000.0, 
+                        "quotes": {
+                            "USD": {
+                                "price": 6378.52, 
+                                "volume_24h": 4515580000.0, 
+                                "market_cap": 109239088451.0, 
+                                "percent_change_1h": 0.61, 
+                                "percent_change_24h": 0.4, 
+                                "percent_change_7d": 3.87
+                            }
+                        }, 
+                        "last_updated": 1530479598
+                */
 
-/* MAIN BODY OF THE SCRIPT STARTS BELOW */
+            }
 
-// Simply greet the user
-print ("USD Cryptocoin Valuations (REST API) [Source: CoinmarketCap]\n");
+            sleep($sleepPeriod); // Sleep to avoid rate-limiting
+         }
 
-// Get a MySQL connection 
-$mysqli_Conn = ConnectTo_MYSQL("127.0.0.1","root","","cryptodev","3306");
+        $i++; // Update our loop count by 1 for every coin provided by the API
+    } while($i < $numCoins); // Loop until we get to the max limit for the amount of coins on CMC.
 
-// Get the JSON array from the source URL
-$coin_List = GetJSON_Array("https://api.coinmarketcap.com/v1/ticker/");
-
-// At this point we have the JSON data. Loop through the array, prepare, and insert or update the data into our database depending if the record already exists
-foreach($coin_List as $coinKey=>$coinValue) {
-	
-	// Attempt to insert the data into a MySQL database, using prepared statements for protection against SQL Injections
-	if ($stmt = $mysqli_Conn->prepare("INSERT INTO coins(`name`,`price_usd`,`timestamp`) VALUES (?,?,?) ON DUPLICATE KEY UPDATE price_usd=?,timestamp=?")) {
-		$stmt->bind_param("sdidi", $coinValue['symbol'], $coinValue['price_usd'], time(), $coinValue['price_usd'],time());
-		
-		// If the execution of the final query fails, it will print the error but continue onto the next coin.
-		if($stmt->execute())
-			print "Successfully inserted/updated " . $coinValue['name'] . " to the database\n";
-		else 
-			print $stmt->error . "\n"; 
-			
-		$stmt->close();
-	}
-	
-}
-
-// The script has executed successfully at this point, close the database connection.
-$mysqli_Conn->close();
-
-print("The script ran successfully. Goodbye\n");
+    function GetJSON_Array($url) { // Fetches a JSON decoded array from a given URL, if unsuccessful it will stop the program
+            
+        echo "Making request to " . $url . "\n";
+        $ch = curl_init();
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Parser");
+        
+        $ch_headers = ['Accept:application/json'];
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $ch_headers);
+        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,  2);
+        
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        $response = curl_exec($ch);
+        
+        if(curl_errno($ch)) {
+            print curl_error($ch) . "\n";
+            die();
+        }
+        
+        return json_decode($response,true);
+    }
 ?>
